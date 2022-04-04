6cbff95593629b70f542059b39254366a09ca591
abhivarma07 <abhijeetthemessi@gmail.com>
2022-04-05 02:41:48 +0530

    integrated Api calls

--- App.js
+++ App.js
@@ -1,13 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React , {useState , useEffect} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -18,95 +9,84 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Axios from 'axios'
+import {Button, Center , Nati, NativeBaseProvider} from 'native-base'
+import User from './components/User';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+// const key = 'smkwn'
+// const URL = 'https://randomuser.me/api/${key}/params'
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const App = () => {
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+
+  const [details, setdetails] = useState(null)
+
+  const fetchDetails = async () => {
+    try{
+    
+      const {data} = await Axios.get('https://randomuser.me/api/')
+      const details = data.results[0]
+
+      console.log(details.email)
+
+      setdetails(details)
+
+
+    } catch (error){
+      console.log(error)
+    }
+  }
+
+
+  useEffect(() => {
+    fetchDetails()
+  }, [])
+
+  if(!details){
+    return (
+      <View style={styles.container}>
+        <Text>Loading..</Text>
+      </View>
+    )
+  }
+
+  else{
+    return(
+      <NativeBaseProvider styles = {styles.container}>
+        <View style = {styles.container}>
+          <View style = {styles.container}>
+            <User/>
+            <Button
+            size = "md"
+            style = {styles.button}
+            onPress = {() => fetchDetails()}
+            colorScheme = "primary"
+            >
+            <Text>Welcome</Text>
+            </Button>
+          </View>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+      </NativeBaseProvider>
+      
+    )
+  }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  
+}
 
 export default App;
+
+const styles = StyleSheet.create({
+  container:{
+    flex : 1 ,
+    justifyContent : "center",
+    alignItems : "center",
+    backgroundColor : "#222831"
+  },
+
+  button : {
+    marginTop : 30,
+    paddingHorizontal : 30 ,
+  }
+})
---
+++ components/User.js
@@ -0,0 +1,39 @@
+import React from "react"
+
+import { Text, View , StyleSheet} from "react-native"
+
+const User = () => {
+    return(
+        <View>
+            <Text>I am a random User</Text>
+        </View>
+    )
+}
+
+export default User ;
+
+const styles = StyleSheet.create({
+    card: {
+      width: '90%',
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      backgroundColor: '#4f8a8b',
+      borderColor: '#4f8a8b',
+      borderWidth: 2,
+    },
+    cardItem: {
+      backgroundColor: '#4f8a8b',
+    },
+    image: {
+      width: 150,
+      height: 150,
+      borderRadius: 150 / 2,
+      overflow: 'hidden',
+      borderWidth: 3,
+      borderColor: '#fbd46d',
+      marginTop: -50,
+    },
+    text: {
+      color: '#eeeeee',
+    },
+  });
